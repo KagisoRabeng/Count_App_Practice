db2eff5775984d360f9e547eef03529a87984454
Rabeng Kagiso <kagisorabeng3@gmail.com>
2025-02-10 16:19:41 +0200

    added previous entries storage

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0
 
 function increment() {
@@ -10,4 +11,10 @@ function increment() {
 
 function save() {
     console.log(count)
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    // 3. Render the variable in the saveEl using innerText
+    // NB: Make sure to not delete the existing content of the paragraph
+    let saved = count + "-" 
+    saveEl.innerText += saved
+
 }
