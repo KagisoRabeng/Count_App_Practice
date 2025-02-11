f9db1b9d60878037f7c49a21f33986e0c6f918ad
Rabeng Kagiso <kagisorabeng3@gmail.com>
2025-02-11 10:10:44 +0200

    reset number to 0 functionality added

--- index.js
+++ index.js
@@ -7,14 +7,10 @@ function increment() {
     countEl.innerText = count
 }
 
-// 1. Create a function, save(), which logs out the count when it's called
-
 function save() {
-    console.log(count)
-    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    // 3. Render the variable in the saveEl using innerText
-    // NB: Make sure to not delete the existing content of the paragraph
+    //console.log(count)
     let saved = count + "-" 
     saveEl.textContent += saved
-
+    countEl.textContent = 0
+    count = 0
 }
